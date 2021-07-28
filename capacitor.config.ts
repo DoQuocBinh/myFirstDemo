a583744cede1378377c6d79b8bc96bf07d9753fd
Binh Do <binhdq@fe.edu.vn>
2021-07-28 10:12:25 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myFirstDemo',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
