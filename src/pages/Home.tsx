f11fb6f8f1ccb16c5655bfe7c461da13438e69cb
Binh Do <binhdq@fe.edu.vn>
2021-07-28 11:54:59 +0700

    Ok

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, 
+    IonContent, IonHeader, 
+    IonPage, IonTitle, IonToast, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [showToast,setShowToast] = useState(false)
+  function clickHandle(){
+    //alert('Button clicked!');
+    setShowToast(true);
+    setTimeout(() => {
+      setShowToast(false)
+    }, 2000);
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -13,9 +24,12 @@ const Home: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle>My First App</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonButton onClick={clickHandle} expand="block"color="warning">Click me!</IonButton>
+        <IonToast isOpen={showToast} 
+              message="Luch time!!" position="middle"></IonToast>
         <ExploreContainer />
       </IonContent>
     </IonPage>
